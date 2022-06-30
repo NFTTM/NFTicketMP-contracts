7ea7efff715c76131a52323a4cf97cc36ca5856c
0xzhiqi <104973959+0xzhiqi@users.noreply.github.com>
2022-06-30 12:24:34 +0800

    Deleted unused imports and updated natspec

--- contracts/NftTicketTest.sol
+++ contracts/NftTicketTest.sol
@@ -1,16 +1,16 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract NftTicket is ERC721, Ownable, ReentrancyGuard {
-    bool public canTransfer;
-    uint256 public tokenId;
-    string public baseURISet;
+/**
+ * @title NFT tickets
+ * @author Team F
+ * @notice You can use this contract to issue event tickets that are non-transferrable
+ * @dev More features eg royalties for organisers will be added in future releases
+ */
+contract NftTicket is ERC721, Ownable {
     struct TicketCategory {
         bytes32 categoryName;
         uint256 ticketPrice;
@@ -22,26 +22,32 @@ contract NftTicket is ERC721, Ownable, ReentrancyGuard {
         bytes32 eventDate;
         bytes32 eventTime;
     }
-    event BuyTicket (address indexed _buyer, bytes32 _ticketCategory);
-    event CheckIn (address indexed _attendee, bool checkedIn);
-    EventDetails public eventDetails;
+    bool public canTransfer;
+    uint256 public tokenId;
+    string public baseURISet;
     mapping(address => bool) public checkedIn;
     mapping(address => bool) public hasBoughtTicket;
     /**
-    @notice maps the ticket category name to the TicketCategory stuct so that
-    price and max no for each ticket category can be queried
-    */
+     * @notice maps the ticket category name to the TicketCategory stuct so that
+     * price and max no for each ticket category can be queried
+     */
     mapping(bytes32 => TicketCategory) public ticketCategoryMapping;
-    
-    // TODO: Consider removing
+    /**
+     * @dev This array is for tracking the ticket categories only. Details in the
+     * ticket categories of the array are not updated during purchase. In the next
+     * version, the array elements will be replaced with just the ticket category
+     * name in bytes32 to avoid any possible confusion
+     */
     TicketCategory[] public ticketCategoryArray;
+    EventDetails public eventDetails;
+    event BuyTicket(address indexed _buyer, bytes32 _ticketCategory);
+    event CheckIn(address indexed _attendee, bool checkedIn);
 
-    constructor(
-        string memory _ticketName,
-        string memory _ticketSymbol
-    ) ERC721(_ticketName, _ticketSymbol){}
+    constructor(string memory _ticketName, string memory _ticketSymbol)
+        ERC721(_ticketName, _ticketSymbol)
+    {}
 
-    function setEventDetails (
+    function setEventDetails(
         bytes32 _eventName,
         bytes32 _eventDate,
         bytes32 _eventTime
@@ -66,55 +72,57 @@ contract NftTicket is ERC721, Ownable, ReentrancyGuard {
         ticketCategoryArray.push(ticketCategoryMapping[_name]);
     }
 
-    // TODO: Consider removing
-    // function getTicketCategoryArraySize() public view returns (uint256 size) {
-    //     size = ticketCategoryArray.length;
-    // }
-
-    /// @dev To test on bsc testnet if this works
-    function getTicketCategoryArraySize() public view returns (uint) {
+    /**
+     * @dev To get the number of ticket categories to iterate through
+     */
+    function getTicketCategoryArraySize() public view returns (uint256) {
         return ticketCategoryArray.length;
     }
 
     /**
-    @notice Each wallet can only buy 1 ticket
-    */
+     * @notice Each wallet can only buy 1 ticket
+     */
     function buyTicket(bytes32 _ticketCategory) public payable {
         require(hasBoughtTicket[msg.sender] == false, "Already bought");
-        TicketCategory storage ticketCategoryBuying = ticketCategoryMapping[_ticketCategory];
+        TicketCategory storage ticketCategoryBuying = ticketCategoryMapping[
+            _ticketCategory
+        ];
         require(
-            ticketCategoryBuying.maxNoOfTickets > ticketCategoryBuying.numberOfTicketsBought,
+            ticketCategoryBuying.maxNoOfTickets >
+                ticketCategoryBuying.numberOfTicketsBought,
             "Sold out!"
         );
-        require (
+        require(
             msg.value >= ticketCategoryBuying.ticketPrice,
             "Please pay for ticket"
         );
         hasBoughtTicket[msg.sender] = true;
-        ticketCategoryBuying.numberOfTicketsBought +=1;
+        ticketCategoryBuying.numberOfTicketsBought += 1;
         tokenId++;
         _safeMint(msg.sender, tokenId);
         emit BuyTicket(msg.sender, _ticketCategory);
     }
 
     /**
-    @dev for organiser to check user in after verifying user's signature
-    */
+     * @dev for organiser to check user in after verifying user's signature
+     */
     function checkAttendeeIn(address _attendeeAddress) public onlyOwner {
         checkedIn[_attendeeAddress] = true;
         emit CheckIn(_attendeeAddress, checkedIn[_attendeeAddress]);
     }
 
     /**
-    @notice for event organiser to allow transfer 
-    */
+     * @notice for event organiser to allow transfer
+     */
     function setCanTransfer(bool _canTransfer) public onlyOwner {
         canTransfer = _canTransfer;
     }
 
     /**
-    @dev bool canTransfer set to disable resale by default
-    */
+     * @dev bool canTransfer set to disable resale by default. Future iterations
+     * would give event organisers the ability to turn on the transfer function
+     * and charge royalties for ticket resale
+     */
     function transferFrom(
         address from,
         address to,
@@ -122,14 +130,19 @@ contract NftTicket is ERC721, Ownable, ReentrancyGuard {
     ) public override {
         require(canTransfer, "Resell not allowed");
         //solhint-disable-next-line max-line-length
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner nor approved");
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: caller is not token owner nor approved"
+        );
 
         _transfer(from, to, tokenId);
     }
 
     /**
-    @dev bool canTransfer set to disable resale by default
-    */
+     * @dev bool canTransfer set to disable resale by default. Future iterations
+     * would give event organisers the ability to turn on the transfer function
+     * and charge royalties for ticket resale
+     */
     function safeTransferFrom(
         address from,
         address to,
@@ -140,8 +153,10 @@ contract NftTicket is ERC721, Ownable, ReentrancyGuard {
     }
 
     /**
-    @dev bool canTransfer set to disable resale by default
-    */
+     * @dev bool canTransfer set to disable resale by default. Future iterations
+     * would give event organisers the ability to turn on the transfer function
+     * and charge royalties for ticket resale
+     */
     function safeTransferFrom(
         address from,
         address to,
@@ -149,13 +164,16 @@ contract NftTicket is ERC721, Ownable, ReentrancyGuard {
         bytes memory data
     ) public override {
         require(canTransfer, "Resell not allowed");
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner nor approved");
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: caller is not token owner nor approved"
+        );
         _safeTransfer(from, to, tokenId, data);
     }
 
-    /** 
-    @dev To set ticket image link from ipfs
-    */
+    /**
+     * @dev To set ticket image link from ipfs
+     */
     function setBaseURI(string memory _ticketImageURL) public onlyOwner {
         baseURISet = _ticketImageURL;
     }
